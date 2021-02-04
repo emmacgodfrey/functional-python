16bcecfdff17413305fe7d11a7e8c15ec2f0e5e2
emmacgodfrey <emma.godfrey@pomona.edu>
2021-02-04 11:41:17 -0800

    try2

--- Fixme.py
+++ Fixme.py
@@ -22,7 +22,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    return [x for x in range(n) if x % 2 == 0]
+    return [x for x in range(n + 1) if x % 2 == 0]
 
 
 def threes(n):
@@ -41,7 +41,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    return [x for x in range(n) if '3' in str(x)]
+    return [x for x in range(n + 1) if '3' in str(x)]
 
 
 def small_words(text):
@@ -61,7 +61,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    return [word for word in text.split() if len(word) < 4]
+    return [word for word in text.split() if len(word) <= 4]
 
 
 def squares(n):
@@ -79,7 +79,7 @@ def squares(n):
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
 
-    return [x*x for x in range(n+1)]
+    return [x*x for x in range(1:n+1)]
 
 
 def lengths(strings):
